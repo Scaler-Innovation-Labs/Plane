bb3f9f77b5bf058ab9cae42392b4549eed0be3ce
RickDeb2004 <debanjanrick04@gmail.com>
2025-03-18 23:39:20 +0530

    Chore:repo setup done

---
+++ packages/i18n/src/types/language.ts
@@ -0,0 +1,6 @@
+export type TLanguage = "en" | "fr" | "es" | "ja" | "zh-CN" | "zh-TW" | "ru" | "it" | "cs" | "sk" | "de" | "ua" | "pl" | "ko";
+
+export interface ILanguageOption {
+  label: string;
+  value: TLanguage;
+}
